731d4b845b2b7dfb01a6203536b8e6ed80a8068b
InsiyaMithaiwala <mithaiwalainsiya7@gmail.com>
2024-10-26 20:33:29 +0530

    first commit

---
+++ src/videos.ts
@@ -0,0 +1,91 @@
+export interface Video {
+  title: string;
+  image: string,
+  thumbImage: string;
+  author: string;
+  views: string;
+  timestamp: string;
+}
+
+export const Videos: Video[] = [
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  },
+  {
+    title: "The Inspiring Story of Steve Jobs and Apple",
+    image: "/photo.jpg",
+    thumbImage: "/profile.jpg",
+    author: "Alex Johnson",
+    views: "5M views",
+    timestamp: "2 months ago"
+  }
+]
